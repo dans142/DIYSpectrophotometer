b112e2c904e32abad74d87d928b1f2dbbdead928
dans142 <59532748+dans142@users.noreply.github.com>
2023-12-23 19:19:47 +0700

    Add files via upload

---
+++ Testing/LDRTest/LDRTest.ino
@@ -0,0 +1,14 @@
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int sensorValue = analogRead(26);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.println(voltage);
+  delay(1000);
+}
